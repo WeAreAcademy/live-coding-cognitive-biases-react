60d97468d253d2f5910803a125a6ac2be2e0209b
nbogie <neillbogie@googlemail.com>
2021-11-02 10:53:00 +0000

    added CognitiveBias element

--- src/components/CognitiveBiases.tsx
+++ src/components/CognitiveBiases.tsx
@@ -3,20 +3,24 @@ import data from '../data/biases.json';
 
 export function CognitiveBiases() {
 
-
-
     return <div>
         <h1>List of Cognitive Biases</h1>
         Loaded {data.length} biases
-        {makeElementsForBiases(data)}
+        {data.map((bias) => <CognitiveBias bias={bias} author='neill' />)}
     </div>;
 }
 
+//@ts-ignore
+function CognitiveBias(props): JSX.Element {
+    const bias = props.bias;
+    return <div
+        key={bias.name}
+        className='bias'
+    >Bias:
+        {bias.name}
+        <div className='description'>{bias.description}</div>
+        {bias.group}
+        hello the author is {props.author}
 
-function makeElementsForBiases(biases: any) {
-    const biasElements = []
-    for (let bias of data) {
-        biasElements.push(<div>{bias.name}</div>);
-    }
-    return biasElements;
+    </div>
 }
