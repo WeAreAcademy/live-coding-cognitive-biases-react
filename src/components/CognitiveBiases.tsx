8aed245fafc0ced41fe7e49eff226aa0e115ec17
nbogie <neillbogie@googlemail.com>
2021-11-04 10:55:13 +0000

    add search bar - does nothing (controlled component)

--- src/components/CognitiveBiases.tsx
+++ src/components/CognitiveBiases.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import importedBiasData from '../data/biases.json';
 import { CognitiveBias } from '../types';
 import { CognitiveBiasView } from './CognitiveBiasView';
@@ -6,8 +6,15 @@ import { CognitiveBiasView } from './CognitiveBiasView';
 const biases: CognitiveBias[] = importedBiasData;
 
 export function CognitiveBiases() {
+    const [searchTerm, setSearchTerm] = useState('');
+    console.log("CognitiveBiases function is called at ", new Date());
     return <div>
         <h1>List of Cognitive Biases</h1>
+        <input
+            onChange={(event) => setSearchTerm(event.target.value)}
+            placeholder='search term...'
+            value={searchTerm} />
+        Current Search term: {searchTerm}<br />
         Loaded {biases.length} biases
         <div className='biases'>
             {biases.map((bias) => <CognitiveBiasView key={bias.name} bias={bias} />)}
